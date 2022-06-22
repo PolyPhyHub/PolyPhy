1429459e3c24ef171eef1690eca57138bcd94165
Patrice Jada <patricemusoke@gmail.com>
2022-06-21 22:28:25 -0400

    Update lib directory

---
+++ polyphy/lib/defaults.py
@@ -0,0 +1,7 @@
+import os
+
+from polyphy._version import __version__
+
+VERSION = __version__
+COPYRIGHT = "Copyright (C) 2022"
+LICENSE = "MIT"
---
+++ polyphy/lib/get_opts.py
@@ -0,0 +1,11 @@
+import argparse
+from polyohy.lib.defaults import VERSION
+
+parser = argparse.ArgumentParser(prog="polyphy")
+parser.add_argument('-v', '--version', action='version', version=VERSION)
+parser.add_argument('-q', '--quiet', help='suppress output', action='store_true')
+
+# subparsers
+
+# polyphy help
+subparsers = parser.add_subparsers(help='sub command help', dest='command')
