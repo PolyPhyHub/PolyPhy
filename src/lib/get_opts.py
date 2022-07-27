1aa746ac02e8a4749180a55fd0879dcc234613bf
Patrice Jada <patricemusoke@gmail.com>
2022-07-26 22:06:56 -0400

    Update get_opts

--- src/lib/get_opts.py
+++ src/lib/get_opts.py
@@ -1,5 +1,5 @@
 import argparse
-from polyphy.src.lib.defaults import VERSION
+from src.lib.defaults import VERSION
 
 parser = argparse.ArgumentParser(prog="src")
 parser.add_argument('-v', '--version', action='version', version=VERSION)
