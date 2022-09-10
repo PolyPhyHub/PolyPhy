431e57414733b9469a8663ee3df12b424bed6323
Patrice Jada <patricemusoke@gmail.com>
2022-09-10 13:49:14 -0400

    Create polyphy main class

---
+++ src/polyphy/lib/simulator.py
@@ -0,0 +1,8 @@
+class Poly:
+    """Superclass for all Polyphys (generic polyphy)
+
+    this class contains all the common variables and functions in polyphy 2D and 3D
+    """
+
+    def run(self):
+        """Run the simulation, overriden by individual classes polyphy 2D and 3D"""
