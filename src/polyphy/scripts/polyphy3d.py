25109185787164f6f217264d48fcd73610a1b58e
Patrice Jada <patricemusoke@gmail.com>
2022-09-10 13:50:20 -0400

    Create polyphy child classes

---
+++ src/polyphy/scripts/polyphy3d.py
@@ -0,0 +1,11 @@
+from ..lib.simulator import Poly
+
+
+class polyphys(Poly):
+    """Polyphy3D simulator"""
+
+    name = "polyphy3d"
+
+    def run(self):
+        """Simulates 3d run"""
+        print("polyphy3d is under development")
