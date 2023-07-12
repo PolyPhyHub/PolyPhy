73a00d6cce0206d85db36c77909c6bfb84010d28
prashant.jha <prashant.jha@media.net>
2023-07-13 00:57:01 +0530

    inheritence segregated

--- experiments/jupyter/production/main.py
+++ experiments/jupyter/production/main.py
@@ -12,19 +12,9 @@ import second
 import third
 import fourth
 import kernels
+import finalKernels
 
-@ti.data_oriented
-class FinalKernels(kernels.Kernels):
-    @ti.kernel
-    def render_visualization(self,deposit_vis: first.FLOAT_GPU, trace_vis: first.FLOAT_GPU, current_deposit_index: first.INT_GPU):
-        print("Prashant's inherit works!")
-        for x, y in ti.ndrange(fourth.vis_field.shape[0], fourth.vis_field.shape[1]):
-            deposit_val = fourth.deposit_field[x * fourth.DEPOSIT_RESOLUTION[0] // fourth.VIS_RESOLUTION[0], y * fourth.DEPOSIT_RESOLUTION[1] // fourth.VIS_RESOLUTION[1]][current_deposit_index]
-            trace_val = fourth.trace_field[x * fourth.TRACE_RESOLUTION[0] // fourth.VIS_RESOLUTION[0], y * fourth.TRACE_RESOLUTION[1] // fourth.VIS_RESOLUTION[1]]
-            fourth.vis_field[x, y] = ti.pow(first.VEC3f(trace_vis * trace_val, deposit_vis * deposit_val, ti.pow(ti.log(1.0 + 0.2 * trace_vis * trace_val), 3.0)), 1.0/2.2)
-        return
-
-k = FinalKernels()
+k = finalKernels.FinalKernels()
 
 ## Initialize GPU fields
 fourth.data_field.from_numpy(fourth.data)
