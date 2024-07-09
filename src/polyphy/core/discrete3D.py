17a2ec2daa543e76cdbd2202a3fbfa19f0bb7833
Abhinav kumar <126642111+Abhinavcode13@users.noreply.github.com>
2024-07-09 21:35:40 +0530

    Fix nested space

--- src/polyphy/core/discrete3D.py
+++ src/polyphy/core/discrete3D.py
@@ -46,6 +46,7 @@ class PPConfig_3DDiscrete(PPConfig):
         self.input_file = ppData.input_file
         def log_info(message, *args):
             Logger.logToStdOut('info', message, *args)
+            
         log_info('Trace grid resolution:', self.TRACE_RESOLUTION)
         log_info('Deposit grid resolution:', self.DEPOSIT_RESOLUTION)
         log_info('Vis resolution:', self.VIS_RESOLUTION)
